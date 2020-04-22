9e089ce7e69150824c388c448bb16d96f8cceaee
Joey <joey.alzamora@gmail.com>
2020-04-22 22:33:33 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  "Hello, my name is Arel."
+end
+
+def badge_batch_creator(array)
+    new_array = [] 
+    array.each do |name|
+      new_array.push("Hello, My name is #{name}.")
+ end
+  new_array
+end
