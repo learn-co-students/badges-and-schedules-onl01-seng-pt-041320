32da33ffd7e72a416042e29086bd7cb9c179c0e6
Harrison Price <hprice518@gmail.com>
2020-04-24 22:09:42 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+ attendees = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(badges)
+speakers = []
+badges.each do |speaker|
+  speakers << badge_maker(speaker)
+  end
+  speakers
+end
+
