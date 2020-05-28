27fbe5c7cd19768f60b6d1fef4cc3e3cb55f37b1
chica25 <patricia@patriciaco.com>
2020-05-28 03:23:44 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,27 +5,37 @@ end
 
 def batch_badge_creator(names)
     array = []
-    names.each { |list| array << "Hello, my name is #{list}."}
+    names.each do |list| array << "Hello, my name is #{list}."
+  end
     array
-  end     
+end     
 
 def assign_rooms(list)
      room = []
-     list.each_with_index { |name, i| room << "Hello, #{name}! You'll be assigned to room #{i + 1}!"}
-     room
+     list.each_with_index do |name, index|
+      room << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
-  
-# def printer(name)
-#   "Hello, my name is #{name}."
+  room
+end
+
+  #with the counter
+  # def assign_rooms(name)
+    #room = []
+#     counter = 1
+#     name.each do |name, counter|
+#      room << "Hello, #{name}! You'll be assigned to room #{counter}!"
+#     counter += 1
+#   end
+#   room 
 # end
-  
-  
+
+
 def printer(name)
     batch_badge_creator(name).each do |list|
       puts list
   end
-  assign_rooms(name).each do |list|
-    puts list
+    assign_rooms(name).each do |list|
+      puts list
   end
 end
   
