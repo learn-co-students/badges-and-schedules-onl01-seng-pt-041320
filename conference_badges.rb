0a15c43e2375533fa7badf923fc7c34454521d65
chica25 <patricia@patriciaco.com>
2020-05-28 03:26:59 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,18 +12,18 @@ def batch_badge_creator(names)
 end     
 
 def assign_rooms(list)
-     room = []
-     list.each_with_index do |name, index|
-      room << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    room = []
+    list.each_with_index do |name, index|
+    room << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
   room
 end
 
   #with the counter
   # def assign_rooms(name)
-    #room = []
-#     counter = 1
-#     name.each do |name, counter|
+      #room = []
+#      counter = 1
+#      name.each do |name, counter|
 #      room << "Hello, #{name}! You'll be assigned to room #{counter}!"
 #     counter += 1
 #   end
